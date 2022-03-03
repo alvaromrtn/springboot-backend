e0129c54b8b60fb89c908ae0f7f74ab19c631a83
alvaromrtn <alvaromrtn@gmail.com>
2022-03-03 21:17:22 +0100

    Profesor_Controller actualizado

--- src/main/java/springboot/controller/Profesor_Controller.java
+++ src/main/java/springboot/controller/Profesor_Controller.java
@@ -1,28 +1,50 @@
 package springboot.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import springboot.entity.Profesor;
-import springboot.repository.Profesor_Repository;
+import springboot.model.Profesor;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin("http://localhost:8081/")
 public class Profesor_Controller {
 
-	@Autowired
-	private Profesor_Repository profesorRepositorio;
-
 	@GetMapping("/profesores")
-	public List<Profesor> buscarEmpleados(){
-		
-		return profesorRepositorio.findAll();
+	public List<Profesor> buscarEmpleados() {
+
+		List<Profesor> profesores = new ArrayList<Profesor>();
+
+		Profesor profesor1 = new Profesor();
+
+		profesor1.setNombre("Nombre1");
+		profesor1.setApellidos("Apellido1");
+		profesor1.setCorreo("Correo1");
+
+		profesores.add(profesor1);
+
+		Profesor profesor2 = new Profesor();
+
+		profesor2.setNombre("Nombre2");
+		profesor2.setApellidos("Apellido2");
+		profesor2.setCorreo("Correo2");
+
+		profesores.add(profesor2);
+
+		Profesor profesor3 = new Profesor();
+
+		profesor3.setNombre("Nombre3");
+		profesor3.setApellidos("Apellido3");
+		profesor3.setCorreo("Correo3");
+
+		profesores.add(profesor3);
+
+		return profesores;
 	}
-	
+
 }

04274aae0c2ad3d26dc52ac83edd809a2a21a1a6
alvaromrtn <alvaromrtn@gmail.com>
2022-03-24 15:34:24 +0100

    Cambios en los servicios

--- src/main/java/springboot/service/grupos/GruposPractica_Service.java
+++ src/main/java/springboot/service/grupos/GruposPractica_Service.java
@@ -28,40 +28,40 @@ import springboot.model.Grupo;
 @Service
 public class GruposPractica_Service {
 
-	public List<Grupo> getGruposPractica(int codigo)
+	public List<Grupo> getGruposPractica(int codigo_asignatura)
 			throws IOException, ParserConfigurationException, SAXException {
-		
+
 		List<Grupo> gruposPractica = new ArrayList<>();
 
-		// 1. Crear dirección de servicio
+		// 1. Crear dirección del servicio
 		URL url = new URL("http://diaweb.usal.es/diaweb/services/Grupos.GruposHttpSoap11Endpoint/");
 
 		// 2. Abrir conexión a la dirección del servicio
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-		// 3. Establecer los parámetros
-		// 3.1 Configuración del modo de envío: GET
+		// 3. Establecer los parámetros:
+		// - 3.1 Configuración del modo de envío: GET
 		connection.setRequestMethod("GET");
 
-		// 3.2 Establecer el formato de datos: tipo de contenido
+		// - 3.2 Establecer el formato de datos: Tipo de contenido
 		connection.setRequestProperty("content-type", "text/xml;charset=utf-8");
 
-		// 3.3 Establecer entrada y salida, la conexión predeterminada recién creada no
-		// tiene permisos de lectura y escritura
+		// - 3.3 Establecer entrada y salida, la conexión predeterminada recién creada
+		// no tiene permisos de lectura y escritura
 		connection.setDoInput(true);
 		connection.setDoOutput(true);
 
-		// 4. Organizar datos SOAP y enviar solicitud
-		String soapXML = getXML_GruposPractica(String.valueOf(codigo));
+		// 4. Crear solicitud SOAP
+		String soapXML = getXML_GruposPractica(String.valueOf(codigo_asignatura));
 
-		/// Enviar la información en una secuencia
+		// 5. Enviar solicitud SOAP en una secuencia
 		OutputStream os = connection.getOutputStream();
 		os.write(soapXML.getBytes());
 
-		// El quinto paso: recibir la respuesta del servidor e imprimir
+		// 6. Recibir respuesta del servidor y guardar datos
 		int responseCode = connection.getResponseCode();
-		if (200 == responseCode) { // indica que el servidor respondió con éxito
-			/// Obtener el flujo de datos devuelto por la solicitud de conexión actual
+		if (200 == responseCode) { // El servidor respondió con éxito
+			// Obtener flujo de datos devuelto por la solicitud de conexión actual
 			InputStream is = connection.getInputStream();
 			InputStreamReader isr = new InputStreamReader(is);
 			BufferedReader br = new BufferedReader(isr);
@@ -79,34 +79,28 @@ public class GruposPractica_Service {
 
 			os.close();
 
-			// RESULTADO:
-			System.out.println("RESULTADO: " + sb.toString());
-			
 			InputSource inputSource = new InputSource(new StringReader(sb.toString()));
-			
+
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			Document document = db.parse(inputSource);
 
 			document.getDocumentElement().normalize();
-			System.out.println("Root Element :" + document.getDocumentElement().getNodeName());
 
 			NodeList nList = document.getElementsByTagName("soapenv:Body");
 
 			Node nNode = nList.item(0);
-			
-			System.out.println("\nCurrent Element :" + nNode.getNodeName());
-			
+
 			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-				
+
 				Element eElement = (Element) nNode;
-				
+
 				int numero_grupos = eElement.getElementsByTagName("ns:return").getLength();
-				
-				for (int n=0; n<numero_grupos; n++) {
-					
+
+				for (int n = 0; n < numero_grupos; n++) {
+
 					Grupo grupo = new Grupo();
-					
+
 					grupo.setId(n);
 					grupo.setCodigoAula(Integer.parseInt(eElement.getElementsByTagName("ax21:codigo_aula").item(n).getTextContent()));
 					grupo.setCodigoGrupo(Integer.parseInt(eElement.getElementsByTagName("ax21:codigo_grupo").item(n).getTextContent()));
@@ -122,27 +116,27 @@ public class GruposPractica_Service {
 					grupo.setNombreProfesor(eElement.getElementsByTagName("ax21:nombre_profesor").item(n).getTextContent());
 					grupo.setNumeroAlumnos(Integer.parseInt(eElement.getElementsByTagName("ax21:numero_alumnos").item(n).getTextContent()));
 					grupo.setPeriodo(eElement.getElementsByTagName("ax21:periodo").item(n).getTextContent());
-					
+
 					gruposPractica.add(grupo);
 				}
 
 			}
-			
+
 		}
-		
+
 		return gruposPractica;
 	}
 
 	String getXML_GruposPractica(String codigo_asignatura) {
 
-		String soapXML = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:ser='http://serviciosweb'>"
-				+ "<soapenv:Header/>"
-				+ "<soapenv:Body>"
-				+ "<ser:gruposPracticaAsignatura>"
-				+ "<ser:codigo_asignatura>" + codigo_asignatura + "</ser:codigo_asignatura>"
-				+ "</ser:gruposPracticaAsignatura>"
-				+ "</soapenv:Body>"
-				+ "</soapenv:Envelope>";
+		String soapXML =	"<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:ser='http://serviciosweb'>" +
+							"<soapenv:Header/>" +
+							"<soapenv:Body>" +
+							"<ser:gruposPracticaAsignatura>" +
+							"<ser:codigo_asignatura>" + codigo_asignatura + "</ser:codigo_asignatura>" +
+							"</ser:gruposPracticaAsignatura>" +
+							"</soapenv:Body>" +
+							"</soapenv:Envelope>";
 
 		return soapXML;
 	}
--- src/main/java/springboot/service/titulaciones/Titulaciones_Service.java
+++ src/main/java/springboot/service/titulaciones/Titulaciones_Service.java
@@ -28,41 +28,39 @@ import springboot.model.Titulacion;
 @Service
 public class Titulaciones_Service {
 
-	public List<Titulacion> getListadoTitulaciones()
-			throws IOException, ParserConfigurationException, SAXException {
-		
+	public List<Titulacion> getListadoTitulaciones() throws IOException, ParserConfigurationException, SAXException {
+
 		List<Titulacion> titulaciones = new ArrayList<>();
 
-		// 1. Crear dirección de servicio
+		// 1. Crear dirección del servicio
 		URL url = new URL("http://diaweb.usal.es/diaweb/services/Titulaciones.TitulacionesHttpSoap11Endpoint/");
 
 		// 2. Abrir conexión a la dirección del servicio
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-		// 3. Establecer los parámetros
-		// 3.1 Configuración del modo de envío: GET
+		// 3. Establecer los parámetros:
+		// - 3.1 Configuración del modo de envío: GET
 		connection.setRequestMethod("GET");
 
-		// 3.2 Establecer el formato de datos: tipo de contenido
+		// - 3.2 Establecer el formato de datos: Tipo de contenido
 		connection.setRequestProperty("content-type", "text/xml;charset=utf-8");
 
-		// 3.3 Establecer entrada y salida, la conexión predeterminada recién creada no
-		// tiene permisos de lectura y escritura
+		// - 3.3 Establecer entrada y salida, la conexión predeterminada recién creada
+		// no tiene permisos de lectura y escritura
 		connection.setDoInput(true);
 		connection.setDoOutput(true);
 
-		// 4. Organizar datos SOAP y enviar solicitud
-		// String soapXML = getXML("211"); // 211 INGENIERIA INFORMATICA
+		// 4. Crear solicitud SOAP
 		String soapXML = getXML_Titulaciones();
 
-		/// Enviar la información en una secuencia
+		// 5. Enviar solicitud SOAP en una secuencia
 		OutputStream os = connection.getOutputStream();
 		os.write(soapXML.getBytes());
 
-		// El quinto paso: recibir la respuesta del servidor e imprimir
+		// 6. Recibir respuesta del servidor y guardar datos
 		int responseCode = connection.getResponseCode();
-		if (200 == responseCode) { // indica que el servidor respondió con éxito
-			/// Obtener el flujo de datos devuelto por la solicitud de conexión actual
+		if (200 == responseCode) { // El servidor respondió con éxito
+			// Obtener flujo de datos devuelto por la solicitud de conexión actual
 			InputStream is = connection.getInputStream();
 			InputStreamReader isr = new InputStreamReader(is);
 			BufferedReader br = new BufferedReader(isr);
@@ -80,46 +78,39 @@ public class Titulaciones_Service {
 
 			os.close();
 
-			// RESULTADO:
-			System.out.println("RESULTADO: " + sb.toString());
-			
 			InputSource inputSource = new InputSource(new StringReader(sb.toString()));
-			
+
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			Document document = db.parse(inputSource);
 
 			document.getDocumentElement().normalize();
-			System.out.println("Root Element :" + document.getDocumentElement().getNodeName());
 
 			NodeList nList = document.getElementsByTagName("soapenv:Body");
 
 			Node nNode = nList.item(0);
-			
-			System.out.println("\nCurrent Element :" + nNode.getNodeName());
-			
+
 			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-				
+
 				Element eElement = (Element) nNode;
-				
+
 				int numero_titulaciones = eElement.getElementsByTagName("ns:return").getLength();
-				
-				for (int n=0; n<numero_titulaciones; n++) {
-					
+
+				for (int n = 0; n < numero_titulaciones; n++) {
+
 					Titulacion titulacion = new Titulacion();
-					
+
 					titulacion.setId(n);
 					titulacion.setCentro(eElement.getElementsByTagName("ax219:centro").item(n).getTextContent());
 					titulacion.setCodigo(Integer.parseInt(eElement.getElementsByTagName("ax219:codigo").item(n).getTextContent()));
 					titulacion.setNombre(eElement.getElementsByTagName("ax219:nombre").item(n).getTextContent());
 					titulacion.setNumeroCursos(Integer.parseInt(eElement.getElementsByTagName("ax219:numero_cursos").item(n).getTextContent()));
-					
 
 					titulaciones.add(titulacion);
 				}
 
 			}
-			
+
 		}
 
 		return titulaciones;
@@ -127,12 +118,12 @@ public class Titulaciones_Service {
 
 	String getXML_Titulaciones() {
 
-		String soapXML = "<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:ser='http://serviciosweb'>"
-				+ "<soapenv:Header/>"
-				+ "<soapenv:Body>"
-				+ "<ser:titulaciones/>"
-				+ "</soapenv:Body>"
-				+ "</soapenv:Envelope>";
+		String soapXML =	"<soapenv:Envelope xmlns:soapenv='http://schemas.xmlsoap.org/soap/envelope/' xmlns:ser='http://serviciosweb'>" +
+							"<soapenv:Header/>" +
+							"<soapenv:Body>" +
+							"<ser:titulaciones/>" +
+							"</soapenv:Body>" +
+							"</soapenv:Envelope>";
 
 		return soapXML;
 	}
